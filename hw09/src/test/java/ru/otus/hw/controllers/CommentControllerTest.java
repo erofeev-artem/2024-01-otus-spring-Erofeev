d17070462bdbde8311ee6c907bf70aea3e8dbec2
artemerofeev <ErofeevAN@bcs.ru>
2024-06-08 23:50:34 +0300

    Homework 09 Spring MVC Completed

---
+++ hw09/src/test/java/ru/otus/hw/controllers/CommentControllerTest.java
@@ -0,0 +1,41 @@
+package ru.otus.hw.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.hw.controller.CommentController;
+import ru.otus.hw.services.AuthorService;
+import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.CommentService;
+import ru.otus.hw.services.GenreService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Контроллер для работы с комментариями")
+@WebMvcTest(CommentController.class)
+public class CommentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BookService bookService;
+    @MockBean
+    private AuthorService authorService;
+    @MockBean
+    private GenreService genreService;
+    @MockBean
+    private CommentService commentService;
+
+    @DisplayName("Добавление нового комментария")
+    @Test
+    void saveComment() throws Exception {
+        this.mockMvc.perform(post("/comment/add")
+                        .queryParam("bookId", "1"))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("/book/info?id=1"));
+    }
+}
